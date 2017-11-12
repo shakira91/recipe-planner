8445f686e60fd43effaa2f11a46118c518d8aeb1
Shakira D <4237135+shakira91@users.noreply.github.com>
2017-11-11 23:19:21 -0500

    commit

--- src/app/questions/questions.component.ts
+++ src/app/questions/questions.component.ts
@@ -16,13 +16,12 @@ export class QuestionsComponent implements OnInit {
   ngOnInit() {
   	this.quizForm = new FormGroup({
   		'qOne' : new FormControl(null, Validators.required),
-  		'qTwo' : new FormControl(null, Validators.required),
-  		'qThree' : new FormControl(null, Validators.required)
+  		'qTwo' : new FormControl(null, Validators.required)
   	});
   }
 
   onSubmit() {
-	this.router.navigate(['/recipes/diet=' + this.quizForm.value.qOne + '&ingr=' + this.quizForm.value.qTwo + '&q=' + this.quizForm.value.qThree]);	
+	this.router.navigate(['/recipes/diet=' + this.quizForm.value.qOne + '&q=' + this.quizForm.value.qTwo]);	
   }
 
 }
--- src/app/shared/http.service.ts
+++ src/app/shared/http.service.ts
@@ -12,7 +12,7 @@ export class HttpService {
   	const headers = new Headers();
     headers.append('Access-Control-Allow-Origin', '*');
     let options = new RequestOptions({ headers: headers });
-  	const recipeUrl = 'https://cors-anywhere.herokuapp.com/https://api.edamam.com/search?'+ params +'&to=30&app_id=2913d905&app_key=78453acb21d06083dfdfea5bf9da7181';
+  	const recipeUrl = 'https://cors-anywhere.herokuapp.com/https://api.edamam.com/search?'+ params +'&to=99&app_id=2913d905&app_key=78453acb21d06083dfdfea5bf9da7181';
 	return this.http.request(recipeUrl, options).map(
 		(response: Response) => {
 			const data = response.json();
