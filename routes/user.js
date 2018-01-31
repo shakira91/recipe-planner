1fe804fb16d3580c3a46f9317af88d6b8aa63701
Shakira D <4237135+shakira91@users.noreply.github.com>
2018-01-30 22:19:52 -0500

    change

---
+++ routes/user.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const router = express.Router();
+
+const User = require('../models/user');
+
+// router.get('/', function(req, res, next){
+// 	user.find();
+
+// });
+
+router.post('/', function(req, res, next) {
+	User.findOne({ username: req.body.username }, 
+		function(err, user) {
+		if (err) {
+			return res.status(401).json({
+				title: 'An error occured',
+				error: err.message
+			});
+		}
+		res.status(200).json({
+			message: 'Success',
+			recipes: user.recipes
+		})
+	});
+});
+
+		
+
+
+
+
+module.exports = router;
