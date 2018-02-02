3cc726ff85943186205175f94c3aad02b365454c
Shakira D <4237135+shakira91@users.noreply.github.com>
2018-02-02 01:16:55 -0500

    change

--- src/app/user/edit-recipe/edit-recipe.component.ts
+++ src/app/user/edit-recipe/edit-recipe.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { FormGroup, FormControl, FormArray, Validators } from '@angular/forms';
+import { Http, Response, RequestOptions, Headers, Jsonp } from '@angular/http';
+import 'rxjs/Rx';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-edit-recipe',
@@ -14,10 +17,22 @@ export class EditRecipeComponent implements OnInit {
   recipeIngredients: string = localStorage.getItem('recipe-ingredients');
   editForm: FormGroup; 
 
-  constructor(private router: Router, private route: ActivatedRoute) { }
+  constructor(private router: Router, private route: ActivatedRoute, private http: Http) { }
   
   saveEdit() {
-    console.log(this.editForm.value)
+    const body = {formData: this.editForm.value, index: localStorage.getItem('recipe-index'), userId: localStorage.getItem('userId')};
+    const headers = new Headers({'Content-Type' : 'application/json'});
+    return this.http.post('http://127.0.0.1:3000/edit', body)
+    .map((response: Response) => response.json())
+    .catch((error: Response) => Observable.throw(error.json())
+    ).subscribe(
+    	(data: any) => {
+        console.log(data)
+  		}, 
+  		(error: any) => {
+  			console.log(error);
+  		}
+    );
   }
 
   editingCanceled() {
