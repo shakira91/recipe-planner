fcbfcd9c2f05cf5f026e8ee53ec6992a3ff73faa
Shakira D <4237135+shakira91@users.noreply.github.com>
2018-02-20 23:22:32 -0500

    bleh

--- src/app/user/add-user-recipe/add-user-recipe.component.ts
+++ src/app/user/add-user-recipe/add-user-recipe.component.ts
@@ -15,9 +15,14 @@ export class AddUserRecipeComponent implements OnInit {
   recipeImage: string = localStorage.getItem('recipe-image');
   recipeTitle: string = localStorage.getItem('recipe-title');
   recipeIngredients: string = localStorage.getItem('recipe-ingredients');
+  newImage: string; 
   addRecipeForm: FormGroup; 
 
   constructor(private router: Router, private route: ActivatedRoute, private http: Http) { }
+
+  uploadImg(image) {
+    this.newImage = image;
+  }
   
   addRecipe() {
     const body = {formData: this.addRecipeForm.value, image: this.recipeImage, title: this.recipeTitle, ingredients: this.recipeIngredients, index: localStorage.getItem('recipe-index'), userId: localStorage.getItem('userId')};
@@ -42,6 +47,7 @@ export class AddUserRecipeComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.newImage = "https://images.pexels.com/photos/8500/food-dinner-pasta-spaghetti-8500.jpg?w=1260&h=750&auto=compress&cs=tinysrgb"; 
     this.addRecipeForm = new FormGroup({
       title : new FormControl(null),
       ingredients : new FormControl(null),
