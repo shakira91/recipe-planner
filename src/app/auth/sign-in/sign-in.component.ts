620e598c874955559e209eea02b600bfeb8d6a7e
Shakira D <4237135+shakira91@users.noreply.github.com>
2018-04-04 23:28:35 -0400

    styles

--- src/app/auth/sign-in/sign-in.component.ts
+++ src/app/auth/sign-in/sign-in.component.ts
@@ -7,6 +7,7 @@ import { AuthServiceService } from '../auth-service.service';
   selector: 'app-sign-in',
   templateUrl: './sign-in.component.html',
   styleUrls: ['./sign-in.component.css']
+  
 })
 export class SignInComponent implements OnInit {
 
