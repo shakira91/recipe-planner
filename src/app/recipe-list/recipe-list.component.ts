931caa8d4ddd7ee1981b3f376d26459e1ee45cec
Shakira D <4237135+shakira91@users.noreply.github.com>
2018-04-18 21:36:24 -0400

    stuff

--- src/app/recipe-list/recipe-list.component.ts
+++ src/app/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, Renderer2 } from '@angular/core';
 import { AuthServiceService } from '../auth/auth-service.service';
 import { Http, Response, RequestOptions, Headers, Jsonp} from '@angular/http';
 import { Router, ActivatedRoute } from '@angular/router';
@@ -13,12 +13,25 @@ export class RecipeListComponent implements OnInit {
   recipes: any;
   recipeDetail: any;
   hovered: boolean = false;
-
-  constructor(private authService: AuthServiceService, private route: ActivatedRoute) { }
+  show: boolean;
+  @ViewChild('recipeWrapper') recipeWrapper;
+  constructor(private authService: AuthServiceService, private route: ActivatedRoute, private renderer: Renderer2) { }
 
   showRecipe(recipe) {
   	this.hovered = true;
   	this.recipeDetail = recipe.recipe;
+  }
+
+  recipesScrolled() {
+    for (var i = 0; i < this.recipeWrapper.nativeElement.children.length; i++) {
+      var bounding = this.recipeWrapper.nativeElement.children[i].getBoundingClientRect()
+      if (bounding.top >= 0 && bounding.left + 500 >= 0 &&
+          bounding.right <= (window.innerWidth || document.documentElement.clientWidth) &&
+          bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight)
+      ) {
+        this.renderer.addClass(this.recipeWrapper.nativeElement.children[i], "show");
+      }
+    }
   }
 
   ngOnInit() {
@@ -28,25 +41,7 @@ export class RecipeListComponent implements OnInit {
   		}
     );
     
-    document.getElementById("recipe-wrapper").addEventListener("scroll", function() {
-          
-      for (var i = 0; i < document.querySelectorAll(".recipe-item").length; i++) {
-          console.log(document.querySelectorAll(".recipe-item")[i])
-         var bounding = document.querySelectorAll(".recipe-item")[i].getBoundingClientRect()
-    if (bounding.top >= 0 && bounding.left + 500 >= 0 &&
-        bounding.right <= (window.innerWidth || document.documentElement.clientWidth) &&
-        bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight)
-    ) {
-       console.log(document.querySelectorAll(".recipe-item")[i])
-    } else {
-        
-    }
-      
-      
-      }
-      
-
-  });
+ 
   }
 
 }
