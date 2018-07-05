20da8efcec2a110f0ca4aaa1df0ec01c172459d8
Shakira D <4237135+shakira91@users.noreply.github.com>
2018-07-05 18:58:56 -0400

    tweaks

--- app.js
+++ app.js
@@ -25,7 +25,6 @@ app.set('view engine', 'hbs');
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, '/dist')));
-app.use(express.static('public'));
 
 app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
